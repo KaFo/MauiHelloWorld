13f8ac5ae49f4a9ecc664cde76bb0e0445fc250f
k.fourmont <k.fourmont@digivod.de>
2023-12-15 07:54:10 +0100

    included maui icon

--- MauiHelloWorld/MauiProgram.cs
+++ MauiHelloWorld/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using MauiIcons.Material;
 
 namespace MauiHelloWorld;
 public static class MauiProgram
@@ -8,12 +9,14 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseMaterialMauiIcons()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
+        
 #if DEBUG
 		builder.Logging.AddDebug();
 #endif

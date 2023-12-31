1777082ac28f6059d5990a1347a6e9b6fe7ceee5
digivod-kf <k.fourmont@digivod.de>
2023-12-31 17:30:09 +0100

    version starting on device with preparations for full libraries, but no autogen static declarations yet in there

--- FFmpeg.Maui.iOS/AppleAssemblyLinks.cs
+++ FFmpeg.Maui.iOS/AppleAssemblyLinks.cs
@@ -23,11 +23,11 @@ using ObjCRuntime;
 
 [assembly: LinkWith("libavutil.a", LinkTarget.Arm64 | LinkTarget.x86_64| LinkTarget.Simulator64, SmartLink = true, ForceLoad = true)]
 
-/*
+
 [assembly: LinkWith("libavcodec.a", LinkTarget.Arm64 | LinkTarget.x86_64, SmartLink = true, ForceLoad = true)]
 
 //problems with bzip2 functions not found
-[assembly: LinkWith("libavformat.a", LinkTarget.Arm64 | LinkTarget.x86_64, SmartLink = true, ForceLoad = true)]
+//[assembly: LinkWith("libavformat.a", LinkTarget.Arm64 | LinkTarget.x86_64, SmartLink = true, ForceLoad = true)]
 
 [assembly: LinkWith("libavfilter.a", LinkTarget.Arm64 | LinkTarget.x86_64, SmartLink = true, ForceLoad = true)]
 
@@ -36,6 +36,6 @@ using ObjCRuntime;
 [assembly: LinkWith("libswscale.a", LinkTarget.Arm64 | LinkTarget.x86_64, SmartLink = true, ForceLoad = true)]
 
 [assembly: LinkWith("libswresample.a", LinkTarget.Arm64 | LinkTarget.x86_64, SmartLink = true, ForceLoad = true)]
-*/
+
 
 #endif
